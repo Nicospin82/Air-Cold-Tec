55de5e5c9e8454b3baae1501e54fa9b64382031f
nico spinelli <nicospinelli88@gmail.com>
2024-09-04 22:43:29 -0300

    add: agregando imagenes al carrusel

---
+++ src/data/instalaciones.js
@@ -0,0 +1,192 @@
+export const instalaciones = [
+    {
+        id: 1,
+        tipo: "Aire Acondicionado tipo split",
+        operacion: "instalacion",
+        provincia: "Buenos Aires",
+        localidad: "Veronica",
+
+    },
+
+    {
+        id: 2,
+
+    },
+
+    {
+        id: 3,
+        tipo: "Aire Acondicionado tipo split",
+        operacion: "instalacion",
+        provincia: "Buenos Aires",
+        localidad: "Veronica",
+
+    },
+
+    {
+        id: 4,
+
+    },
+
+    {
+        id: 5,
+        tipo: "Aire Acondicionado tipo split",
+        operacion: "instalacion",
+        provincia: "Buenos Aires",
+        localidad: "Pipinas",
+
+    },
+
+    {
+        id: 6,
+
+    },
+
+    {
+        id: 7,
+        tipo: "Aire Acondicionado tipo split",
+        operacion: "instalacion",
+        provincia: "Buenos Aires",
+        localidad: "Veronica",
+
+    },
+
+    {
+        id: 8,
+    },
+
+    {
+        id: 9,
+        tipo: "Aire Acondicionado tipo split",
+        operacion: "instalacion",
+        provincia: "Buenos Aires",
+        localidad: "Atalaya",
+
+    },
+
+    {
+        id: 10,
+
+    },
+
+    {
+        id: 11,
+        tipo: "Aire Acondicionado tipo split",
+        operacion: "instalacion",
+        provincia: "Buenos Aires",
+        localidad: "Veronica",
+
+    },
+
+    {
+        id: 12,
+
+    },
+
+    {
+        id: 13,
+        tipo: "Aire Acondicionado tipo split",
+        operacion: "instalacion",
+        provincia: "Buenos Aires",
+        localidad: "Magdalena",
+
+    },
+
+    {
+        id: 14,
+    },
+
+    {
+        id: 15,
+        tipo: "Aire Acondicionado tipo split",
+        operacion: "instalacion",
+        provincia: "Buenos Aires",
+        localidad: "Veronica",
+
+    },
+
+    {
+        id: 16,
+    },
+
+    {
+        id: 17,
+        tipo: "Aire Acondicionado tipo split",
+        operacion: "instalacion",
+        provincia: "Buenos Aires",
+        localidad: "Veronica",
+
+    },
+
+    {
+        id: 18,
+    },
+
+    {
+        id: 19,
+        tipo: "Aire Acondicionado tipo split",
+        operacion: "instalacion",
+        provincia: "Buenos Aires",
+        localidad: "Magdalena",
+
+    },
+
+    {
+        id: 20,
+
+    },
+
+    {
+        id: 21,
+        tipo: "Aire Acondicionado tipo split",
+        operacion: "instalacion",
+        provincia: "Buenos Aires",
+        localidad: "Veronica",
+
+    },
+
+    {
+        id: 22,
+
+    },
+
+    {
+        id: 23,
+        tipo: "Aire Acondicionado tipo split",
+        operacion: "instalacion",
+        provincia: "Buenos Aires",
+        localidad: "Veronica",
+
+    },
+
+    {
+        id: 24,
+    },
+
+    {
+        id: 25,
+        tipo: "Aire Acondicionado tipo split",
+        operacion: "instalacion",
+        provincia: "Buenos Aires",
+        localidad: "Magdalena",
+
+    },
+
+    {
+        id: 26,
+    },
+
+    {
+        id: 27,
+        tipo: "Aire Acondicionado tipo split",
+        operacion: "instalacion",
+        provincia: "Buenos Aires",
+        localidad: "Veronica",
+
+
+    },
+
+    {
+        id: 28,
+    },
+
+];
