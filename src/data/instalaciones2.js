de207fd0bc396d133ce2d0a6fc8f48449656107d
nico spinelli <nicospinelli88@gmail.com>
2025-01-18 11:50:10 -0300

    add:carrusel

---
+++ src/data/instalaciones2.js
@@ -0,0 +1,118 @@
+export const instalaciones2 = [
+{
+    id: 14,
+    tipo: "Aire Acondicionado tipo split",
+    operacion: "instalacion",
+    provincia: "Buenos Aires",
+    localidad: "Vieytes",
+
+},
+
+{
+    id: 15,
+    tipo: "Aire Acondicionado tipo split",
+    operacion: "instalacion",
+    provincia: "Buenos Aires",
+    localidad: "Vieytes",
+
+},
+
+{
+    id: 16,
+    tipo: "Aire Acondicionado tipo split",
+    operacion: "instalacion",
+    provincia: "Buenos Aires",
+    localidad: "Vieytes",
+
+},
+
+{
+    id: 17,
+    tipo: "Aire Acondicionado tipo split",
+    operacion: "instalacion",
+    provincia: "Buenos Aires",
+    localidad: "Veronica",
+
+},
+
+{
+    id: 18,
+    tipo: "Aire Acondicionado tipo split",
+    operacion: "instalacion",
+    provincia: "Buenos Aires",
+    localidad: "Veronica",
+
+},
+
+{
+    id: 19,
+    tipo: "Aire Acondicionado tipo split",
+    operacion: "instalacion",
+    provincia: "Buenos Aires",
+    localidad: "Vieytes",
+
+},
+
+{
+    id: 20,
+    tipo: "Aire Acondicionado tipo split",
+    operacion: "limpieza",
+    provincia: "Buenos Aires",
+    localidad: "Veronica",
+
+},
+
+{
+    id: 21,
+    tipo: "Aire Acondicionado tipo split",
+    operacion: "instalacion",
+    provincia: "Buenos Aires",
+    localidad: "Veronica",
+
+},
+
+{
+    id: 22,
+    tipo: "Aire acondicionado split",
+    operacion: "instalacion",
+    provincia: "Buenos Aires",
+    localidad: "Veronica",
+
+},
+
+{
+    id: 23,
+    tipo: "Aire Acondicionado tipo split",
+    operacion: "instalacion",
+    provincia: "Buenos Aires",
+    localidad: "Veronica",
+
+},
+
+{
+    id: 24,
+    tipo: "Aire Acondicionado tipo split",
+    operacion: "instalacion",
+    provincia: "Buenos Aires",
+    localidad: "Punta indio",
+
+},
+
+{
+    id: 25,
+    tipo: "Aire Acondicionado tipo split",
+    operacion: "instalacion",
+    provincia: "Buenos Aires",
+    localidad: "Veronica",
+
+},
+
+{
+    id: 26,
+    tipo: "Aire Acondicionado tipo split",
+    operacion: "instalacion",
+    provincia: "Buenos Aires",
+    localidad: "Veronica",
+
+},
+]
